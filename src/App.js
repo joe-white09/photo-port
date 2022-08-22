1721d539c747e6ad562993afcfe10917568e73fb
joe-white09 <joe_white09@yahoo.com>
2022-08-22 12:10:51 -0500

    test for about

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+
   return (
-   <div>
-    <Nav></Nav>
-    <main>
-      <About></About>
-    </main>
-   </div>
+    <div>
+      <Nav />
+      <main>
+          <About />        
+      </main>
+    </div>
   );
 }
 
